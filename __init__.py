d07c5f15df92b99efcff8d07ae89b459e6f3fbda
VolkerVollverstrahlt <volkervollverstrahlt@protonmail.com>
2022-12-14 18:04:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,21 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
+from adapt.intent import IntentBuilder
+from database import Database
 
 
 class Timemannage(MycroftSkill):
+    
+    
+    
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.db = Database()
 
-    @intent_file_handler('timemannage.intent')
+
+    @intent_handler(IntentBuilder("read")
+                    .require('what')
+                    .require('todos')
+                    .optinally('todo_name'))
     def handle_timemannage(self, message):
         self.speak_dialog('timemannage')
 
